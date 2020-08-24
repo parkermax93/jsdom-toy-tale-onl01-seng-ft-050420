39626e09469da5cf51823e3c7c2c43f83f100662
Parker Williamson <parker.williamson@gmail.com>
2020-08-24 03:01:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container');
   addBtn.addEventListener('click', () => {
-    // hide & seek with the form
     addToy = !addToy
     if (addToy) {
       toyForm.style.display = 'block'
